228ef7465be3a4a505cf647289c5688e58c5db63
JiHyoungHAN <gkswlgud4343@hufs.ac.kr>
2023-05-31 19:11:59 +0900

    script change

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -10,12 +10,19 @@ using Debug = UnityEngine.Debug;
 public class Spawner : MonoBehaviour, IDropHandler
 {
     public static Spawner instance { get; private set; }
-    void Awake() => instance = this;
 
     [SerializeField]
     private GameObject[] character;
     [SerializeField]
-    private AstronautItem[] itemSO;
+    private AstronautItem[] AstronautSO;
+
+    CoinManager coinManager;
+
+    private void Awake()
+    {
+        instance = this;
+        coinManager = GameObject.Find("CoinManager").GetComponent<CoinManager>();
+    }
 
     public void OnDrop(PointerEventData eventData)
     {
@@ -23,14 +30,16 @@ public class Spawner : MonoBehaviour, IDropHandler
        var item = DragHandeler.itemBeingDragged;
        if (item != null)
        {
-                for (int i = 0; i < itemSO.Length; i++)
+                for (int i = 0; i < AstronautSO.Length; i++)
                 {
-                    if (itemSO[i].name == item.name)
+                    if (AstronautSO[i].name == item.name)
                     {
-                        Debug.Log(itemSO[i].name);
+                        //Debug.Log(itemSO[i].name);
                         Instantiate(character[i], new Vector3(-8, 0, 0), transform.rotation);
-                        character[i].GetComponent<Astronaut>().AllocateItem(itemSO[i]);
+                        character[i].GetComponent<Astronaut>().AllocateItem(AstronautSO[i]);
                     }
+                    int num = 0;
+                    coinManager.DecreaseCoin(num);
                 }
        }
         
